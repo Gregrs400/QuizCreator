9a16c7c96b6d0491f1e34ff50baa7c99281e3591
Greg Salisbury <gregsalisbury25@gmail.com>
2025-08-07 20:17:08 -0400

    Added JFrame

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,16 @@
+import javax.swing.*;
+
 public class Main
 {
 
     public static void main(String[] args)
     {
+
+        JFrame frame = new JFrame("Quiz Creator");
+        frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        frame.setVisible(true);
 
 
 
