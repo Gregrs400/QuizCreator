cc211057d6a12d4a661444f1b35df9a44c698a2e
gregs <gregsalisbury25@gmail.com>
2025-08-23 10:32:30 -0400

    Added questions and method documentation

--- src/Quiz.java
+++ src/Quiz.java
@@ -1,2 +1,16 @@
-public class Quiz {
+import java.util.ArrayList;
+
+public class Quiz
+{
+
+    private final ArrayList<Question> questions = new ArrayList<>();
+
+    // open file
+
+    // file to string
+
+    // parse string into data structure
+
+    // convert data structure into quiz object
+
 }
