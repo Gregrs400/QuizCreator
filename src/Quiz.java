c8faed5c0e85c8490b45769123d27ef4f0722297
gregs <gregsalisbury25@gmail.com>
2025-08-23 10:36:57 -0400

    Added file access code

--- src/Quiz.java
+++ src/Quiz.java
@@ -1,3 +1,5 @@
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class Quiz
@@ -6,6 +8,35 @@ public class Quiz
     private final ArrayList<Question> questions = new ArrayList<>();
 
     // open file
+
+    public File createOrAccessFile(String filePathAndName) {
+
+        try {
+
+            File file = new File(filePathAndName);
+
+            if (file.createNewFile()) {
+
+                System.out.println("file created");
+
+            } else {
+
+                System.out.println("file already created");
+
+            }
+
+            return file;
+
+        } catch (IOException e) {
+
+            System.out.println("IOException occurred");
+            e.printStackTrace();
+
+            return null;
+
+        }
+
+    }
 
     // file to string
 
